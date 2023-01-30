c03d50765c07051f5d7f1bbdc5d45ca4adfd4689
Mike Schmidt (Schmidty) <mschmidty@gmail.com>
2023-01-30 12:35:47 -0700

    added csv action fixed tabs

--- r_files/test.R
+++ r_files/test.R
@@ -1,8 +1,12 @@
 library(dplyr)
 library(ggplot2)
+library(readr)
 gg<-iris%>% 
-      ggplot(aes(Species, Petal.Length))+ 
-      geom_boxplot()+ 
-      geom_jitter()
-
+  ggplot(aes(Species, Petal.Length))+ 
+  geom_boxplot()+ 
+  geom_jitter()
 ggsave("r_files/output/test2.jpg", plot=gg, width=2000, height=1200, units="px")
+
+iris%>%
+  select(Species, Petal.Length)%>%
+  write_csv("r_files/output/test.csv")
